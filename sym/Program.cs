070b4df92c8d051349d4107035cf283045f17398
Артём <shilov2245@gmail.com>
2022-02-10 14:05:58 +0300

    сумма

--- sym/Program.cs
+++ sym/Program.cs
@@ -7,7 +7,9 @@ namespace sym
 		static void Main(string[] args)
 		{
 			Console.WriteLine(args.Length);
-				Console.WriteLine(args[0]+args[1]);
+			int sym_1 = Convert.ToInt32(args[0]);
+			int sym_2= Convert.ToInt32(args[1]);
+			Console.WriteLine(sym_1+ sym_2);
 		}
 	}
 }
