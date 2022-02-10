29146a61af4449583516e205dd1e2bdd994e0806
Артём <shilov2245@gmail.com>
2022-02-10 14:02:01 +0300

    сумма

---
+++ sym/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sym
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(args.Length);
+				Console.WriteLine(args[0]+args[1]);
+		}
+	}
+}
