0b3dcfe14e3f1781572ab9efa041850f6b06b281
Артём <shilov2245@gmail.com>
2022-02-10 14:15:14 +0300

    сумма

--- sym/Program.cs
+++ sym/Program.cs
@@ -6,6 +6,7 @@ namespace sym
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("mysym");
 			Console.WriteLine(args.Length);
 			int sym_1 = Convert.ToInt32(args[0]);
 			int sym_2= Convert.ToInt32(args[1]);
