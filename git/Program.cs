dcf3b15a567c59d96c27c6d14263258e6b9d5c33
Артём <shilov2245@gmail.com>
2022-02-10 13:23:41 +0300

    приветствие

--- git/Program.cs
+++ git/Program.cs
@@ -6,7 +6,7 @@ namespace git
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("MyHello");
 			String name;
 			name = Console.ReadLine();
 			Console.WriteLine($"Привет, {name}");
